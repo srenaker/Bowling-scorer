37d5be5e1dfb0bb2f4f13c202a6300393e56212d
srenaker <renaker@yahoo.com>
2009-07-12 22:28:53 -0700

    first commit

---
+++ game.rb
@@ -0,0 +1,66 @@
+class Game
+  
+  def current_score(state)
+
+    framecount = 1
+    score = 0
+    has_spare = false
+    has_strike = false
+    has_2_strikes = false
+    
+
+    state.each do |frame|
+
+      if (framecount < 10)
+        frame_total = frame[0] + frame[1]
+      else
+        frame_total = frame[0] + frame[1] + frame[2]
+      end
+
+      if (has_spare)        
+        score += (frame_total + frame[0])
+        has_spare = false
+      elsif (has_strike)
+ 
+        score += (frame_total * 2)
+ 
+        if (frame[0] == 10)
+          has_2_strikes = true
+          has_strike = false
+        else
+          has_strike = false 
+        end
+
+      elsif (has_2_strikes)
+        if (framecount < 10)
+          score += ((frame[0] * 3) + (frame[1] * 2))  
+        else
+          score += ((frame[0] * 3) + (frame[1] * 2) + frame[2])  
+        end 
+        
+        if (frame[0] == 10)
+          has_2_strikes = true
+        else
+          has_strike = false 
+          has_2_strikes = false 
+        end
+    
+      else  
+        score += frame_total
+      end
+
+      if (frame[0] == 10 && has_2_strikes == false)
+        has_strike = true
+
+      elsif (frame_total == 10 && frame[0] != 10)
+        has_spare = true
+      end
+
+      framecount += 1
+
+    end
+    score
+  end
+  
+end
+
---
+++ game_spec.rb
@@ -0,0 +1,87 @@
+require "rubygems"
+require "spec"
+
+require "game"
+
+describe Game do
+  
+  describe "score" do
+    
+    it "calculates current score after one frame" do
+      state = [[2,5]]
+      Game.new.current_score(state).should == 7
+    end
+    
+    it "calculates current score after one frame, with spare" do
+      state = [[5,5]]
+      Game.new.current_score(state).should == 10
+    end
+    
+    it "calculates current score after one frame, with strike" do
+      state = [[10,0]]
+      Game.new.current_score(state).should == 10
+    end    
+    
+    it "calculates current score after several frames" do
+      state = [[2,5], [5,3], [4,4], [2,1]]      
+      Game.new.current_score(state).should == 26
+    end
+
+    it "calculates current score after several frames, spare included" do
+      state = [[2,5], [5,5], [4,4], [2,1]]      
+      Game.new.current_score(state).should == 32
+    end
+    
+    it "calculates current score after several frames, strike included" do
+      state = [[2,5], [10,0], [4,4], [2,1]]      
+      Game.new.current_score(state).should == 36
+    end    
+    
+    it "calculates current score after several frames, spare and strike included" do
+      state = [[2,5], [10,0], [4,4], [2,8], [3,5]]      
+      Game.new.current_score(state).should == 54
+    end        
+    
+    it "calculates current score after several frames, with consecutive spare and strike" do
+      state = [[2,5], [9,1], [10,0], [4,4], [2,1]]      
+      Game.new.current_score(state).should == 56
+    end        
+    
+    it "calculates current score after several frames, with 2 consecutive strikes" do
+      state = [[10,0], [10,0], [2,1]]      
+      Game.new.current_score(state).should == 38
+    end            
+
+    it "calculates current score after several frames, with 3 consecutive strikes (TURKEY!)" do
+      state = [[10,0], [10,0], [10,0], [0,9]]      
+      Game.new.current_score(state).should == 78
+    end            
+    
+    it "calculates current score after several frames, with 4 consecutive strikes" do
+      state = [[10,0], [10,0], [10,0], [10,0], [4,3]]      
+      Game.new.current_score(state).should == 108
+    end            
+    
+    it "calculates current score for a full game, with confusingly mixed strikes and spares" do
+      state = [[10,0], [3,4], [10,0], [1,9], [10,0], [10,0], [5,5], [4,4], [8,2], [10,4,6]]      
+      Game.new.current_score(state).should == 171
+    end                
+    
+    it "calculates current score for a full game, with mixed strikes and spares" do
+      state = [[10,0], [3,4], [10,0], [1,9], [10,0], [10,0], [5,5], [4,4], [8,2], [10,10,10] ]     
+      Game.new.current_score(state).should == 181
+    end
+
+    it "calculates current score for a full game, with mixed strikes and spares" do
+      state = [[10,0], [3,4], [10,0], [1,9], [10,0], [10,0], [5,5], [4,4], [8,2], [3,7,8] ]     
+      Game.new.current_score(state).should == 162
+    end
+
+    it "calculates current score for a perfect game" do
+      state = [[10,0], [10,0], [10,0], [10,0], [10,0], [10,0], [10,0], [10,0], [10,0], [10,10,10] ]     
+      Game.new.current_score(state).should == 300
+    end
+
+  end
+end	
+	
