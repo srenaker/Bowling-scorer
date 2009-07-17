c7c66d266ea4b3d9d1f855547954ac0f18a5ef0b
Stephen Renaker <srenaker@mbpro.local>
2009-07-17 13:42:00 -0700

    refactoring: made new Frame object

---
+++ frame.rb
@@ -0,0 +1,28 @@
+class Frame
+  
+  def initialize(frame, framecount)
+    @frame = frame
+    @framecount = framecount
+    
+  end
+
+  def validate_frame
+
+    # disallow rolls > 10    
+
+    @frame[0] = 10 if (@frame[0] > 10) 
+    @frame[1] = 10 if (@frame[1] > 10)
+    if (@framecount == 10)
+      @frame[2] = 10 if (@frame[2] > 10)
+    end
+
+    # if first roll is a strike, discount 2nd roll, unless it's the 10th @frame
+    if (@framecount < 10)
+      @frame[1] = 0 if (@frame[0] == 10)
+    end
+    
+    @frame
+  end
+  
+  
+end
--- game.rb
+++ game.rb
@@ -1,3 +1,5 @@
+require "frame"
+
 class Game
   
   def score(state)
@@ -10,7 +12,7 @@ class Game
 
     state.each do |frame|
       
-      frame = validate_frame(frame, framecount)
+      frame = Frame.new(frame, framecount).validate_frame
 
       frame_total = frame.inject(0) { |x,n| x+n }
 
@@ -59,23 +61,6 @@ class Game
     score
   end
   
-  def validate_frame(frame, framecount)
-
-    # disallow rolls > 10    
-
-    frame[0] = 10 if (frame[0] > 10) 
-    frame[1] = 10 if (frame[1] > 10)
-    if (framecount == 10)
-      frame[2] = 10 if (frame[2] > 10)
-    end
-
-    # if first roll is a strike, discount 2nd roll, unless it's the 10th frame
-    if (framecount < 10)
-      frame[1] = 0 if (frame[0] == 10)
-    end
-    
-    frame
-  end
   
 end
 
